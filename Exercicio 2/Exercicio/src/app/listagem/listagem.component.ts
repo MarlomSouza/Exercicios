ba5deec186b65b687f5589b6ae3ce92f0f7e7c12
MarlomSouza <marlom1012@gmail.com>
2018-01-05 14:27:08 -0300

    ajuste pesquisa usuario e mensagem quando usuario não existir

--- Exercicio 2/Exercicio/src/app/listagem/listagem.component.ts
+++ Exercicio 2/Exercicio/src/app/listagem/listagem.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Http } from "@angular/http";
+import { Response } from '@angular/http/src/static_response';
+import { HttpResponse } from 'selenium-webdriver/http';
+import { Headers } from '@angular/http/src/headers';
 
 @Component({
   moduleId: module.id,
@@ -8,10 +11,11 @@ import { Http } from "@angular/http";
 })
 export class ListagemComponent implements OnInit {
 
-  @Input() search: string;
+  @Input() search: string = "";
 
   usuarios: Object[] = [];
   http: Http;
+  links: Link[] = []
 
   constructor(http: Http) {
     this.http = http;
@@ -23,27 +27,78 @@ export class ListagemComponent implements OnInit {
 
   obterTodosUsuarios(){
     this.http
-    .get('https://api.github.com/users')
-    .map(result => result.json())
-    .subscribe(usuarios => { this.usuarios = usuarios; console.log(usuarios) }), error => console.log("ERRO ====>>> ", error);
+    .get('https://api.github.com/users?per_page=10&client_id=e2f8d5f949ac1b6d9717&client_secret=38c8e714d646a07455ba50741135b16f110016f4')
+    .subscribe(usuarios => 
+      { this.usuarios = usuarios.json(); 
+        this.parse_link_header(usuarios.headers.get("link"));
+      }), error => console.log("ERRO ====>>> ", error);
   };
 
   searchUser() {
-    console.log("this.search.length",this.search.length);
+    
     if(this.search.length ==0){
       this.obterTodosUsuarios();
       return;
     }
+    console.log("CHEGOU AQUI");
+    if(this.search.length < 3)
+      return;
 
     this.http
       .get('https://api.github.com/search/users?type=Users&q='+this.search +'&client_id=e2f8d5f949ac1b6d9717&client_secret=38c8e714d646a07455ba50741135b16f110016f4')
-      .map(result => result.json())
-      .subscribe(usuarios => { this.usuarios = usuarios.items;}), error => console.log("ERRO ====>>> ", error);
+      .subscribe(usuarios => { 
+        this.usuarios = usuarios.json().items; 
+         this.parse_link_header(usuarios.headers.get("link"));
+      }), error => console.log("ERRO ====>>> ", error);
+
   }
 
+  obterMaisUsuarios(){
+    console.log("this.links", this.links.find((v) => v.nome == "next").url);
+    this.http
+    .get(this.links.find((link) => link.nome == "next").url)
+    .subscribe(usuarios => 
+      {
+        if(this.search.length ==0)
+          this.usuarios = this.usuarios.concat(usuarios.json());   
+        else
+          this.usuarios = this.usuarios.concat(usuarios.json().items);   
 
+          this.parse_link_header(usuarios.headers.get("link"));
+      }), error => console.log("ERRO ====>>> ", error);
+  };
 
+   parse_link_header(header:string) {
+    this.links =[];
 
+    if (header.length == 0) 
+      throw new Error("input must not be of zero length");
 
+    // Split parts by comma
+    let parts = header.split(',');
+    
+    // Parse each part into a named link
+    for(let i=0; i< parts.length; i++){
+      var section = parts[i].split(';');
+      if (section.length != 2) {
+        throw new Error("section could not be split on ';'");
+      }
+      
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      let name = section[1].replace(/rel="(.*)"/, '$1').trim();
 
+      this.links.push( new Link(name, url));
+      }
+    };
+}
+
+export class Link{
+    nome: string;
+    url: string;
+
+    constructor(nome, url){
+      this.nome = nome;
+      this.url = url;
+    };
+    
 }
