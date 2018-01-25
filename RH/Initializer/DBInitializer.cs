9d1f9686506b3d6e9eba202614c46005905fa19e
Marlom Souza <marlom.souza@before.com.br>
2018-01-25 13:18:55 -0300

    db initialize tecnologia

---
+++ RH/Initializer/DBInitializer.cs
@@ -0,0 +1,32 @@
+
+using System.Linq;
+using RH.Models;
+
+namespace Initializer.Data
+{
+    public static class DBInitializer
+    {
+        public static void Initialize(Context context)
+        {
+            context.Database.EnsureCreated();
+            InitializeTecnologias(context);
+            context.SaveChanges();
+        }
+
+        private static void InitializeTecnologias(Context context)
+        {
+            if (context.Tecnologias.Count() == 0)
+                return;
+
+            var tecnologias = new Tecnologia[]{
+                new Tecnologia{Id=0, Nome= "Tecnologia 1"},
+                new Tecnologia{Id=0, Nome= "Tecnologia 2"},
+                new Tecnologia{Id=0, Nome= "Tecnologia 3"},
+                new Tecnologia{Id=0, Nome= "Tecnologia 4"},
+                new Tecnologia{Id=0, Nome= "Tecnologia 5"}
+            };
+
+            context.Tecnologias.Concat(tecnologias);
+        }
+    }
+}
--- RH/Program.cs
+++ RH/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using Initializer.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using RH.Models;
 
 namespace RH
 {
@@ -7,7 +11,25 @@ namespace RH
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<Context>();
+                    DBInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
